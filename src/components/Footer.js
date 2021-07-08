15debc746b0dcac90d3d42e10e4c5fde54329196
aniket965 <aniket965.as@gmail.com>
2021-07-08 13:53:13 +0530

    fix:mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Footer() {
   return (
-    <div className="center-child absolute bottom-8 left-0 right-0 footer z-20 ">
+    <div className="center-child absolute bottom-2 left-0 right-0 footer z-20 md:bottom-4">
       <div className="footer-section-1 z-20 flex flex-row ">
         <div className="flex items-center justify-items-center">
           <img
@@ -10,7 +10,7 @@ export default function Footer() {
             src="./time_icon.svg"
           />
         </div>
-        <div  >STAY TUNED FOR MORE!</div>
+        <div   >STAY TUNED FOR MORE!</div>
       </div>
       {/* <div className="footer-section-2 z-20 ">
         <div>TWITTER</div>
--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,12 +1,12 @@
 import Button from "./Button"
 export default function HeadLine() {
   return (
-    <div className="head-line center-child mt-24 " >
-        <div className="text-7xl head-line-section " >
+    <div className="head-line center-child mt-48 md:mt-24" >
+        <div className=" head-line-section text-2xl md:text-6xl " >
         A cross-L2 communication protocol for a multi-L2 future.
         </div>
 
-        <div className="social-section mt-4" >
+        <div className="social-section mt-2 md:mt-4" >
             <Button text="Contact us" />
             <Button text="Follow us on Twitter" outline={true} />
         </div>
