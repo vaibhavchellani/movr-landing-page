619bcb3354c89af191bfa77189a2a472eadbe8ec
aniket965 <aniket965.as@gmail.com>
2021-11-15 20:57:58 +0530

    feat: adds link to discord, fund movr and career page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,19 @@ export default function Footer() {
   return (
     <div className="center-child absolute bottom-2 left-0 right-0 footer z-20 md:bottom-4">
       <div className="footer-section-1 z-20 flex flex-row ">
-        <div className="flex items-center justify-items-center">
+        {/* <div className="flex items-center justify-items-center">
           <img
             className="w-5 font-medium h-5 mx-2 justify-self-center"
             src="./time_icon.svg"
           />
         </div>
-        <div   >STAY TUNED FOR MORE!</div>
+        <div   >STAY TUNED FOR MORE!</div> */}
       </div>
-      {/* <div className="footer-section-2 z-20 ">
-        <div>TWITTER</div>
-        <div>FACEBOOK</div>
-        <div>LINKDIN</div>
-        <div>MAILUS</div>
-      </div> */}
+      <div className="footer-section-2 z-20 ">
+        <div  onClick={()=> window.open('https://app.fund.movr.network')} >FUNDMOVR </div> |
+        <div  onClick={()=> window.open(' https://movrnetwork.notion.site/We-should-work-together-5cc360ba8df94399948c573a21833804')} >CAREER</div>|
+        <div  onClick={()=> window.open('https://discord.com/invite/UFgG72rp42')} >DISCORD</div> 
+      </div>
       <div className="footer-section-3 z-20 ">
         Movr Network - © 2021 All Rights Reserved.
       </div>
