8d5aeb71c2b155acf06aa802fa9fd4ea76c15dc9
aniket965 <aniket965.as@gmail.com>
2022-02-02 17:35:36 +0530

    feat: oc up movr

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -3,11 +3,11 @@ export default function HeadLine() {
   return (
     <div className="overflow-hidden zerome absolute flex items-center justify-center  head-line center-child" >
         <div className=" head-line-section text-2xl md:text-3xl lg:text-6xl " >
-        A cross-L2 communication protocol for a multi-L2 future
+      Movr is now <img className="inline-block h-36"  src='./socket.png'/>
         </div>
 
         <div className="social-section mt-2 md:mt-4" >
-            <Button  onClick={()=> window.open('mailto:contact@movr.network')} text="Contact us" />
+            <Button  onClick={()=> window.open('https://socket.tech')} text="Visit Socket.tech" />
             <Button onClick={()=> window.open('https://twitter.com/movrnetwork')} text="Follow us on Twitter" outline={true} />
         </div>
     </div>
